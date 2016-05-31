f3a0d94742ac4cdd70340b324d0f01a43c1e0777
John M Chan <jmc009@UCSD.EDU>
2016-05-31 16:29:12 -0700

    WIP on collectable spawning still, need to get the random spawning code from master before continuing

--- src/server/engine/CollectableSpawner.cpp
+++ src/server/engine/CollectableSpawner.cpp
@@ -1,6 +1,32 @@
 #include "CollectableSpawner.h"
 
-void CollectableSpawner::SpawnRandomCollectable(btDiscreteDynamicsWorld* curworld)
+void CollectableSpawner::SpawnRandomCollectable(btDiscreteDynamicsWorld* curworld, int current_tick)
 {
+	// determine whether or not we should spawn
+	if (last_spawn + wait_period != current_tick)
+		return;
+
+	// determine how many to spawn
+	int num_spawns = (rand() % (MAX_NUM_SPAWNS + MIN_NUM_SPAWNS)) + MIN_NUM_SPAWNS;
+
+	for(int i = 0; i < num_spawns; i++)\
+	{
+		// determine which weapon and spawn the number of weapons
+		switch (rand() % NUM_WEAPON_TYPES)
+		{
+			case CollectableType::SEEDGUN:
+			{
+				break;
+			}
+			default:
+			{
+				break;
+			}
+		}
+	}
+
+	// schedule the next spawn
+	last_spawn = current_tick;
+	wait_period = (rand() % (MAX_SPAWN_PERIOD - MIN_SPAWN_PERIOD)) + MIN_SPAWN_PERIOD;
 
 }
--- src/server/engine/CollectableSpawner.h
+++ src/server/engine/CollectableSpawner.h
@@ -1,7 +1,12 @@
 #pragma once
 #include "EntitySpawner.h"
 
-static const int NUM_WEAPON_TYPES = 1;
+static const int NUM_WEAPON_TYPES = 1; // number of types of weapons there are
+static const int STARTING_TICK = 100;  // the tick to start spawning collectables on
+static const int MIN_NUM_SPAWNS = 2;   // Minumum number of collectables spawned for this spawn period
+static const int MAX_NUM_SPAWNS = 4;   // Maximum number of collectables spawned for this spawn period
+static const int MIN_SPAWN_PERIOD = 400;   //  The minimum number of ticks before a new collectable spawns
+static const int MAX_SPAWN_PERIOD = 700;   //  The maximum number of ticks before a new collectable spawns
 
 enum CollectableType {
 	SEEDGUN
@@ -9,8 +14,20 @@ enum CollectableType {
 
 class CollectableSpawner {
 
+private:
+	CollectableSpawner() { last_spawn = 0; wait_period = STARTING_TICK; }
+	~CollectableSpawner() {}
+	unsigned int last_spawn; // the last tick of when a collectable was spawned
+	unsigned int wait_period; // Wait period before next collectable spawn
+
 public:
+	static CollectableSpawner* instance()
+	{
+		static CollectableSpawner* instance = new CollectableSpawner();
+		return instance;
+	}
+
 	// The world to spawn the collectable in
-	void SpawnRandomCollectable(btDiscreteDynamicsWorld* curworld);
+	void SpawnRandomCollectable(btDiscreteDynamicsWorld* curworld, int current_tick);
 
 };
